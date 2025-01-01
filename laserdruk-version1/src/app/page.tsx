43d3888d446f8e11020452da4e5fe2992d6bef18
rafa7lako <rafa7lako@gmail.com>
2025-01-01 20:14:16 +0100

    reworked the cards

---
+++ laserdruk-version1/src/app/page.tsx
@@ -0,0 +1,58 @@
+import heroImage from "../../public/Frame6.jpg";
+
+export default function Home() {
+	return (
+		<div className="max-w-[1200px] m-auto">
+			<nav className="mt-8 w-full bg-neutral-900 text-slate-50 flex justify-between items-center rounded-xl drop-shadow-md">
+				<a href="#" className="px-4 ">
+					Logo
+				</a>
+				<div>
+					
+				</div>
+				<ul className="flex justify-center items-center py-2">
+					<li>
+						<a href="#" className="mx-6 px-2">
+							Home
+						</a>
+					</li>
+					<li>
+						<a href="#" className="mx-6 px-2">
+							Sklep
+						</a>
+					</li>
+					<li>
+						<a href="#" className="mx-6 px-2">
+							Blog
+						</a>
+					</li>
+					<li className="bg-orange-400">
+						<a href="#" className="mx-6">
+							Kontakt
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<main className="w-full h-full">
+				<section className="grid grid-cols-4 grid-rows-8 gap-2 h-[600px] mt-4 drop-shadow-md">
+					<div className="bg-hero-img bg-cover col-start-1 col-end-4 row-span-full rounded-xl">
+            <h1 className="font-display uppercase text-7xl text-slate-50 text-right pt-4 pr-3">Co drukujemy,<br/> mordziaty?</h1>
+          </div>
+
+					<div className="col-start-4 col-end-5 row-start-1 row-end-3 bg-neutral-300 rounded-xl flex justify-center items-center drop-shadow-md hover:cursor-pointer bg-car-wrap bg-cover text-white before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:rounded-xl before:content-['']">
+						<p className="font-medium uppercase z-10">Reklama na pojazdach</p>
+					</div>
+					<div className="col-start-4 col-end-5 row-start-3 row-end-5 bg-neutral-300 rounded-xl flex justify-center items-center drop-shadow-md hover:cursor-pointer bg-shirt-print bg-cover before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:content-[''] text-white before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:rounded-xl before:content-['']">
+						<p className="font-medium uppercase z-10">Nadruki na odzieży</p>
+					</div>
+					<div className="col-start-4 col-end-5 row-start-5 row-end-7 bg-neutral-300 rounded-xl flex justify-center items-center drop-shadow-md hover:cursor-pointer bg-stamp-img bg-cover before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:content-[''] text-white before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:rounded-xl before:content-['']">
+						<p className="font-medium uppercase z-10">Pieczątki</p>
+					</div>
+					<div className="col-start-4 col-end-5 row-start-7 row-end-9 bg-orange-500 rounded-xl flex justify-center items-center drop-shadow-md hover:cursor-pointer">
+						<p className="font-medium text-white uppercase">Skontaktuj się z nami</p>
+					</div>
+				</section>
+			</main>
+		</div>
+	);
+}
---
+++ laserdruk-version1/tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      'display': ['Oswald', 'sans-serif'],
+      'body': ['PT_Sans', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        'hero-img': "url('/Frame6.jpg')",
+        'car-wrap': "url('/carWrap.jpg')",
+        'shirt-print': "url('/shirt.jpg')",
+        'stamp-img': "url('/stamp.jpg')"
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
