df4282c547146762ceedda918058d9e4e82a7a0d
rafa7lako <rafa7lako@gmail.com>
2025-01-16 21:11:41 +0100

    i'm reworking the layout

--- laserdruk-version1/src/app/page.tsx
+++ laserdruk-version1/src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
-import logoImage from '../../public/las3.png'
+import logoImage from "../../public/las3.png";
 
 export default function Home() {
 	return (
 		<div className="max-w-[1200px] m-auto">
 			<nav className="mt-8 w-full bg-neutral-900 text-slate-50 flex justify-between items-center rounded-xl drop-shadow-md overflow-hidden">
 				<a href="#" className="px-4 ">
-					<Image src={logoImage} width={120} height={120} alt="logo laserdruk"/>
+					<Image
+						src={logoImage}
+						width={120}
+						height={120}
+						alt="logo laserdruk"
+					/>
 				</a>
 				<div></div>
 				<ul className="flex justify-center items-center ">
@@ -32,30 +37,26 @@ export default function Home() {
 					</li>
 				</ul>
 			</nav>
-			<main className="w-full h-full">
-				<section className="grid grid-cols-4 grid-rows-8 gap-2 h-[600px] mt-4 drop-shadow-md">
-					<div className="bg-hero-img bg-cover col-start-1 col-end-4 row-span-full rounded-xl">
-						<h1 className="font-display uppercase text-7xl text-slate-50 text-right pt-4 pr-3">
+			<main className="w-full h-full relative">
+				<section className="grid grid-cols-6 grid-rows-8 gap-2 h-[600px] mt-4 drop-shadow-md rounded-xl">
+					<div className="col-start-1 col-end-4 row-span-full bg-slate-100 rounded-xl">
+						
+					</div>
+					<div className=" col-start-4 col-end-7 row-span-full bg-slate-100 rounded-xl">
+						
+
+						<h1 className="font-display uppercase text-7xl text-orange-500 text-right pt-4 pr-3">
 							Co drukujemy,
 							<br /> mordziaty?
 						</h1>
 					</div>
-
-					<div className="col-start-4 col-end-5 row-start-1 row-end-3 bg-neutral-300 rounded-xl flex justify-center items-center drop-shadow-md hover:cursor-pointer bg-car-wrap bg-cover text-white before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:rounded-xl before:content-['']">
-						<p className="font-medium uppercase z-10 ">Reklama na pojazdach</p>
-					</div>
-					<div className="col-start-4 col-end-5 row-start-3 row-end-5 bg-neutral-300 rounded-xl flex justify-center items-center drop-shadow-md hover:cursor-pointer bg-shirt-print bg-cover before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:content-[''] text-white before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:rounded-xl before:content-['']">
-						<p className="font-medium uppercase z-10 ">Nadruki na odzieży</p>
-					</div>
-					<div className="col-start-4 col-end-5 row-start-5 row-end-7 bg-neutral-300 rounded-xl flex justify-center items-center drop-shadow-md hover:cursor-pointer bg-stamp-img bg-cover before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:content-[''] text-white before:absolute before:inset-0 before:bg-black before:bg-opacity-50 before:rounded-xl before:content-['']">
-						<p className="font-medium uppercase z-10 ">Pieczątki</p>
-					</div>
-					<div className="col-start-4 col-end-5 row-start-7 row-end-9 bg-orange-600 hover:bg-orange-500 rounded-xl flex justify-center items-center drop-shadow-md hover:cursor-pointer">
-						<p className="font-medium text-white uppercase ">
-							Skontaktuj się z nami
-						</p>
-					</div>
+					
 				</section>
+				<section className="grid grid-cols-6 grid-rows-8 h-[400]">
+					<div className="row-start-2 row-end-9 col-start-1 col-end-9 bg-slate-400 rounded-xl"></div>
+					<div className="col-start-3 col-end-5 bg-red-100 row-start-1 row-end-3">nigga</div>
+				</section>
+			
 			</main>
 		</div>
 	);
--- laserdruk-version1/tailwind.config.ts
+++ laserdruk-version1/tailwind.config.ts
@@ -1,30 +1,35 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    fontFamily: {
-      'display': ['Oswald', 'sans-serif'],
-      'body': ['PT_Sans', 'sans-serif'],
-      'roboto': ['Roboto', 'sans-serif']
-    },
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      backgroundImage: {
-        'hero-img': "url('/Frame6.jpg')",
-        'car-wrap': "url('/carWrap.jpg')",
-        'shirt-print': "url('/shirt.jpg')",
-        'stamp-img': "url('/stamp.jpg')",
-        'logo': "url('/las3.png')"
-      }
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		fontFamily: {
+			display: ["Oswald", "sans-serif"],
+			body: ["PT_Sans", "sans-serif"],
+			roboto: ["Roboto", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			backgroundImage: {
+				"hero-img": "url('/Frame6.jpg')",
+				"car-wrap": "url('/carWrap.jpg')",
+				"shirt-print": "url('/shirt.jpg')",
+				"stamp-img": "url('/stamp.jpg')",
+				logo: "url('/las3.png')",
+			},
+			addUtilities: {
+				"clip-polygon-custom": {
+					clipPath: "polygon(0 0, 100% 0, 100% 85%, 76% 85%, 76% 100%, 0 100%)",
+				},
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
